93c34fb74fc6cd51493356fcd559d5af40778252
Sani Rogers <sanirogers138@gmail.com>
2023-05-13 16:00:45 -0400

    finish pricing page

--- client/src/components/pricing/SubscriptionCard.tsx
+++ client/src/components/pricing/SubscriptionCard.tsx
@@ -16,12 +16,12 @@ export const SubscriptionCard: React.FC<SubscriptionCardProps> = ({ title, price
         <div className={styles.subscriptionCardContainer}>
             <h3>{title}</h3>
             <h4>${subType === "monthly" ? `${price}/mo` : `${price}/yr`}</h4>
-            {discount && <span>Save ${discount}</span>}
+            {discount && <h6>Save ${discount}</h6>}
             <ul>
                 {features.map(feature => <li>{feature}</li>)}
             </ul>
             {trial && <button>7-Day Free Trial</button>}
-            <button>Buy "{title}"</button>
+            {price === 0 ? <button>Go!</button> : <button>Buy "{title}"</button>}
         </div>
     )
 }
