e74f267fde85eab333626d99fb98aef9a98e3a94
Sani Rogers <sanirogers138@gmail.com>
2023-05-19 15:30:34 -0400

    set icons for sidebar

--- client/src/pages/NoteTakingPage.tsx
+++ client/src/pages/NoteTakingPage.tsx
@@ -1,8 +1,12 @@
+import { HiOutlineDocumentAdd, HiOutlineFolderAdd, HiOutlineUserAdd, HiOutlineUserCircle, HiOutlineViewGrid, HiViewList } from "react-icons/hi";
 import { FolderIcon } from "../components/notes/FolderIcon";
 import { NoteCard } from "../components/notes/NoteCard";
 import styles from "./NoteTakingPage.module.scss";
+import { useState } from "react";
+import { TiSortAlphabeticallyOutline } from "react-icons/ti";
 
 export const NoteTakingPage = () => {
+    const [isListView, setIsListView] = useState(false);
 
     return (
         <div className={styles.notePageWrapper}>
@@ -49,11 +53,18 @@ export const NoteTakingPage = () => {
 
             <div className={styles.optionsContainer}>
                 <div className={styles.optionSidebar}>
-
+                    <HiOutlineDocumentAdd size={35} color="010C80"/>
+                    <HiOutlineFolderAdd size={35} color="010C80"/>
+                    <HiOutlineUserAdd size={35} color="010C80"/>
+                    {isListView ? <HiOutlineViewGrid size={35} color="010C80" /> : <HiViewList size={35} color="010C80" />}
+                    <HiOutlineUserCircle size={35} color="010C80"/>
+                    <TiSortAlphabeticallyOutline size={35} color="010C80"/>
                 </div>
-                <div className={styles.userSidebar}>
-                    
-                </div>
+                {/* <div className={styles.userSidebar}>
+                    <img src="https://baconmockup.com/640/360" alt="user profile" />
+                    <img src="https://baconmockup.com/640/360" alt="user profile" />
+                    <img src="https://baconmockup.com/640/360" alt="user profile" />
+                </div> */}
             </div>
 
             {/* <div className={styles.usersContainer}>
