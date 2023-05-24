189bf823ff2c22d9a8f29bfc90d97a25d9c79d33
Sani Rogers <sanirogers138@gmail.com>
2023-05-24 16:31:41 -0400

    work on modals

--- client/src/components/projects/ViewProjectModal.tsx
+++ client/src/components/projects/ViewProjectModal.tsx
@@ -1,18 +1,19 @@
 import { Modal } from "react-bootstrap";
 import styles from "./ViewProjectModal.module.scss";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { hideViewProjectModal, selectIsViewProjectModalShown } from "../../store/slices/projectSlice";
+import { hideViewProjectModal, selectIsViewProjectModalShown, selectSelectedProject } from "../../store/slices/projectSlice";
 
 export const ViewProjectModal: React.FC = () => {
 
     const selectViewProjectModalState = useAppSelector(selectIsViewProjectModalShown)
+    const selectedProjectState = useAppSelector(selectSelectedProject)
     const dispatch = useAppDispatch()
     return(
         <Modal show={selectViewProjectModalState} onHide={() => dispatch(hideViewProjectModal())} centered>
             <Modal.Header closeButton><h4>Blah</h4></Modal.Header>
             <Modal.Body>
                 <div className={styles.infoContainer}>
-                    <h5>500 Notes</h5>
+                    <h5>{(selectedProjectState && selectedProjectState.notes) ? selectedProjectState.notes.length + "Notes" : 0} </h5>
                     <h5>Created At: 05/18/00</h5>
                     <h5>Updated At: 05/18/00</h5>
                 </div>
